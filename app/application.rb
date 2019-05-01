bab6022305d1a3a05490267c4c7585a7075997ff
Scott Silverman <scottms724@gmail.com>
2019-05-01 17:54:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -28,7 +28,7 @@ class Application
         @@cart << search_term
         resp.write "added #{search_term}"
       end 
-    elsif handle_search(search_term) = false 
+    elsif handle_search(search_term) == false 
       resp.write "We don't have that item"
     end
 
