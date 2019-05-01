cf34c30da06d8a1e262959fd094cc5aa3d304ef6
Scott Silverman <scottms724@gmail.com>
2019-05-01 17:50:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
         resp.write "Your cart is empty"
       else 
       @@cart.each do |cart_item|
-        resp.write "#{cart_item}"
+        resp.write "#{cart_item}\n"
       end 
     elsif req.path.match(/search/)
       search_term = req.params["q"]
