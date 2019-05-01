8142beb74186e047dc5b874da7dd1afc7d91c561
Scott Silverman <scottms724@gmail.com>
2019-05-01 17:43:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,6 +11,9 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    elsif req.path.match(/cart/)
+      @@cart.each do |cart_item|
+        resp.write "#{cart_item}"
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
